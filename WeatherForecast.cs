f4fac9a4737f8472fd60ee77a183c36a96052e13
Kamilia <kamilia.mimouni@polytechnicien.tn>
2023-02-13 09:56:29 +0100

    Ajoutez des fichiers projet.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Predict_prices_using_regression
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
